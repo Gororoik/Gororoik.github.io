a031fc93ff47c13ea76f4e5efe118357d9aaa62c
Gor <gororoik1@mail.ru>
2023-03-04 15:20:59 -0500

    almost done w burger

--- portfolio/js/script.js
+++ portfolio/js/script.js
@@ -18,6 +18,19 @@ document.addEventListener('scroll', (event) => {
    scroll();
 })
 
+document.querySelector('.menu__link').addEventListener('click', (event) => {
+   event.menuLangClicked = true;
+   if (document.querySelector('.menu__langs').classList.contains('menu__langs-show')) {
+      document.querySelector('.menu__langs').classList.remove('menu__langs-show');
+      document.querySelector('.menu__langs').classList.add('menu__langs-unshow');
+   } else if (document.querySelector('.menu__langs').classList.contains('menu__langs-unshow')) {
+      document.querySelector('.menu__langs').classList.remove('menu__langs-unshow');
+      document.querySelector('.menu__langs').classList.add('menu__langs-show');
+   } else if (!document.querySelector('.menu__langs').classList.contains('menu__langs-unshow') && !document.querySelector('.menu__langs').classList.contains('menu__langs-show')) {
+      document.querySelector('.menu__langs').classList.add('menu__langs-show');
+   }
+});
+
 document.querySelector('.lan').addEventListener('click', (event) => {
    event.langClicked = true;
    if (document.querySelector('.langs').classList.contains('langs-show')) {
@@ -32,10 +45,28 @@ document.querySelector('.lan').addEventListener('click', (event) => {
 });
 
 document.body.addEventListener('click', (event) => {
-   if (event.langClicked || event.target.classList.contains('lang') || event.target.classList.contains('a')) return;
+   if (event.target.classList.contains("menu__item") || event.target.classList.contains("underline__title") ) {
+      menuBtn.classList.toggle('menu-btn_unshow')
+      menu.classList.toggle('active');
+      menuBtnClose.classList.toggle('menu-close_show');
+   };
+   if (event.langClicked || event.target.classList.contains('lang') || event.target.classList.contains('a') || event.menuLangClicked || event.target.classList.contains('menu__lang') ) {
+      return;
+   };
    document.querySelector('.langs').classList.remove('langs-show');
    document.querySelector('.langs').classList.add('langs-unshow');
-})
+   document.querySelector('.menu__langs').classList.remove('menu__langs-show');
+   document.querySelector('.menu__langs').classList.add('menu__langs-unshow');
+   // if (event.target.classList.contains("menu__item") || event.target.classList.contains("underline__title")) {
+   //    console.log("gagulo");
+   //    menu.classList.toggle('active');
+   // };
+   // if (event.target.classList.contains("menu__item") || event.target.classList.contains("underline__title")) {
+   //    menuBtn.classList.toggle('menu-btn_unshow')
+   //    menu.classList.toggle('active');
+   //    menuBtnClose.classList.toggle('menu-close_show');
+   // };
+});
 
 let menuBtnClose = document.querySelector('.menu-close');
 let menuBtn = document.querySelector('.menu-btn');
@@ -51,3 +82,27 @@ menuBtnClose.addEventListener('click', function() {
    menuBtnClose.classList.toggle('menu-close_show');
 });
 
+let menuItems = document.querySelectorAll('.menu__item')
+
+
+// function gago() {
+   
+//    menuItems.addEventListener('click', (allItems) => {
+//       for (item of allItems) {
+//          if (item.target.classList.contains("menu__item") || item.target.classList.contains("underline__title")) {
+//             menuBtn.classList.toggle('menu-btn_unshow')
+//             menu.classList.toggle('active');
+//             menuBtnClose.classList.toggle('menu-close_show');
+//          };
+//       };
+//    })
+// }
+// menuItems.addEventListener('click', (allItems) => {
+//    for (item of allItems) {
+//       if (item.target.classList.contains("menu__item") || item.target.classList.contains("underline__title")) {
+//          menuBtn.classList.toggle('menu-btn_unshow')
+//          menu.classList.toggle('active');
+//          menuBtnClose.classList.toggle('menu-close_show');
+//       };
+//    };
+// })
